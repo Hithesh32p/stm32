fe2ff5db82771bd85f5d747e4c9a32cde553e148
Hithesh32p <nopoodd341@gmail.com>
2026-03-26 20:19:26 +0530

    build for stm32c031

--- startup.s
+++ startup.s
@@ -1,14 +1,17 @@
 .syntax unified
-.cpu cortex-m3
+.cpu cortex-m0plus
 .thumb
 
-.global _start
+.global _estack
 .global Reset_Handler
 
-.section .text
-_start:
-    B Reset_Handler
+_estack = 0x20002000   /* 8KB RAM for STM32C031 */
 
+.section .isr_vector,"a",%progbits
+.word _estack
+.word Reset_Handler
+
+.section .text
 Reset_Handler:
     BL main
     B .
